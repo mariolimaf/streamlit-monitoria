61fd25cf0489308137c87fbd21e42e546b2e1b01
mariolimaf <mariolimaff@gmail.com>
2025-05-16 13:59:47 -0300

    Remover prompt de dúvida

--- pages/6_Plantão Tira Dúvidas.py
+++ pages/6_Plantão Tira Dúvidas.py
@@ -5,7 +5,7 @@ st.title("Plantão Tira Dúvidas")
 st.write('## Em caso de dúvidas, estou disponível no Whatsapp')
 st.link_button("Link para chamar no whatsapp", "https://wa.me/5585987240678")
 
-prompt = st.chat_input("Escreva Sua Dúvida")
-if prompt:
-    with open('duvidas/duvidas.txt', 'w') as txt:
-        txt.readlines(prompt)
+# prompt = st.chat_input("Escreva Sua Dúvida")
+# if prompt:
+#     with open('duvidas/duvidas.txt', 'w') as txt:
+#         txt.readlines(prompt)
